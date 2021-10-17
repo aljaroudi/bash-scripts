ea0a6e20a8ed3620ca46afee349f444c7783df34
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2021-10-17 16:00:40 -0500

    Setup command

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -1,9 +1,6 @@
 # Install Xcode tools
 xcode-select --install 
 
-# Clone this repo
-git clone https://github.com/aljaroudi/dotfiles.git ~/.dotfiles
-
 # symlinks to the repo's dotfiles
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# bash setup
+# My macOS Dev Setup
+
+## Setup
+
+```bash
+# Clone this repo
+git clone https://github.com/aljaroudi/dotfiles.git ~/.dotfiles
+# Run the script
+sh ~/Dotfiles.sh
+```
