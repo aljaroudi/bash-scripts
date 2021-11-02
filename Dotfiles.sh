aee22fe59fb2aad25290a6e66945e0983340308b
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2021-11-01 19:39:13 -0500

    Add brew to PATH

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -8,5 +8,9 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Add brew to PATH
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Pass brew location
 brew bundle --file ~/.dotfiles/Brewfile
